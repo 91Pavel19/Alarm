e8fe19875e0992c6563da3fae7f1f4d319de0e44
91Pavel19 <kardaviltsev.p@mail.ru>
2023-08-24 12:33:42 +0300

    Initial commit

---
+++ Alarm/InterfaceAlarm.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Net.Mime;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+using Timer = System.Threading.Timer;
+
+
+namespace Alarm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class InterfaceAlarm : Window
+    {
+
+        public InterfaceAlarm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            Settings settings = new Settings();
+            settings.Show();
+        }
+    }
+}
---
+++ Alarm/Settings.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Alarm;
+
+public partial class Settings : Window
+{
+    public Settings()
+    {
+        InitializeComponent();
+    }
+}
